2c94e57956129fdecb0b3fa493dc3b7add61a52e
ryo-kitashoji <ryo.kitashoji1998@gmail.com>
2023-11-26 22:42:47 +0900

    過去の制作物を追加

---
+++ checkbox/js/script4.js
@@ -0,0 +1,11 @@
+const isAgreed = document.querySelector('#check');
+const btn = document.querySelector('#btn');
+
+isAgreed,addEventListener('change', () => {
+    
+    if (isAgreed.checked){
+        btn.disabled = false;        
+    }else{
+        btn.disabled = true;
+    }
+});
